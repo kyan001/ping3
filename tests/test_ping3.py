a45e37770082afbc00985c6e6b5087868c6888ff
Kyan <ykyankai@gmail.com>
2018-07-31 16:12:34 +0800

    update testcases for bind(src_addr, 0)

--- tests/test_ping3.py
+++ tests/test_ping3.py
@@ -1,15 +1,17 @@
 import sys
+import os.path
 import io
 import unittest
 from unittest.mock import patch
+import socket
 
-sys.path.insert(0, '../')
+sys.path.insert(0, os.path.dirname(sys.path[0]))
 import ping3
 
 
 class test_ping3(unittest.TestCase):
     """ping3 unittest"""
-    __version__ = '1.1.0'
+    __version__ = '1.2.1'
 
     def setUp(self):
         pass
@@ -33,6 +35,11 @@ class test_ping3(unittest.TestCase):
         self.assertIsInstance(delay, float)
         self.assertTrue(delay > 1)
 
+    def test_ping_bind(self):
+        my_ip = socket.gethostbyname(socket.gethostname())
+        delay = ping3.ping('example.com', src_addr=my_ip)
+        self.assertIsInstance(delay, float)
+
     def test_verbose_ping_normal(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             ping3.verbose_ping('example.com')
@@ -46,6 +53,12 @@ class test_ping3(unittest.TestCase):
     def test_verbose_ping_count(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             ping3.verbose_ping('example.com', count=1)
+            self.assertRegex(fake_out.getvalue(), r'.*[0-9]+ms.*')
+
+    def test_verbose_ping_bind(self):
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            my_ip = socket.gethostbyname(socket.gethostname())
+            ping3.verbose_ping('example.com', src_addr=my_ip)
             self.assertRegex(fake_out.getvalue(), r'.*[0-9]+ms.*')
 
 
