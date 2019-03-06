d2c76e7d2a1a27597e56b8d1cffe7e4af8a995c4
Kyan <ykyankai@gmail.com>
2019-03-07 00:29:11 +0800

    update to new version

--- tests/test_ping3.py
+++ tests/test_ping3.py
@@ -12,7 +12,7 @@ import errors  # noqa: linter (pycodestyle) should not lint this line.
 
 class test_ping3(unittest.TestCase):
     """ping3 unittest"""
-    __version__ = "2.0.2"
+    __version__ = "2.0.3"
 
     def setUp(self):
         pass
