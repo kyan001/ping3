4474d76189b379613d568eeec55e0cebd2d41f06
Kyan <ykyankai@gmail.com>
2019-03-06 16:31:31 +0800

    rewrite the logic codes, add size of payload as a optional arg. Add DEBUG mode and EXCEPTION mode.

--- ping3.py
+++ ping3.py
@@ -7,162 +7,212 @@ import select
 import time
 import threading
 
-__version__ = "1.4.1"
+import errors
+from enums import ICMP_DEFAULT_CODE, IcmpType, IcmpTimeExceededCode, IcmpDestinationUnreachableCode
+
+__version__ = "2.0.1"
+DEBUG = False  # DEBUG: Show debug info for developers. (default False)
+EXCEPTIONS = False  # EXCEPTIONS: Raise exception when delay is not available.
+
+IP_HEADER_FORMAT = "!BBHHHBBHII"
+ICMP_HEADER_FORMAT = "!BBHHH"  # According to netinet/ip_icmp.h. !=network byte order(big-endian), B=unsigned char, H=unsigned short
+ICMP_TIME_FORMAT = "!d"  # d=double
+
 
 if sys.platform == "win32":
-    # On Windows, the best timer is time.clock()
-    default_timer = time.clock
+    default_timer = time.clock  # On Windows, the best timer is time.clock()
 else:
-    # On most other platforms the best timer is time.time()
-    default_timer = time.time
-
-# From /usr/include/linux/icmp.h; your milage may vary.
-ICMP_ECHO_REQUEST = 8  # Seems to be the same on Solaris.
+    default_timer = time.time  # On most other platforms the best timer is time.time()
 
 
-def checksum(source_string):
+def _debug(*args):
+    if DEBUG:
+        message = "[DEBUG]"
+        print(message, *args)
+
+
+def ones_comp_sum16(num1: int, num2: int) -> int:
+    """Calculates the 1's complement sum for 16-bit numbers.
+
+    Args:
+        num1: 16-bit number.
+        num2: 16-bit number.
+
+    Returns:
+        The calculated result.
     """
-    I'm not too confident that this is right but testing seems
-    to suggest that it gives the same answers as in_cksum in ping.c
+
+    carry = 1 << 16
+    result = num1 + num2
+    return result if result < carry else result + 1 - carry
+
+
+def checksum(source: bytes) -> int:
+    """Calculates the checksum of the input bytes.
+
+    RFC1071: https://tools.ietf.org/html/rfc1071
+    RFC792: https://tools.ietf.org/html/rfc792
+
+    Args:
+        source: The input to be calculated.
+
+    Returns:
+        Calculated checksum.
     """
+    if len(source) % 2:  # if the total length is odd, padding with one octet of zeros for computing the checksum
+        source += b'\x00'
     sum = 0
-    countTo = len(source_string)
-    count = 0
-    while count < countTo:
-        thisVal = source_string[count + 1] * 256 + source_string[count]
-        sum = sum + thisVal
-        count = count + 2
-
-    if countTo < len(source_string):
-        sum = sum + ord(source_string[len(source_string) - 1])
-
-    sum = (sum >> 16) + (sum & 0xffff)
-    sum = sum + (sum >> 16)
-    answer = ~sum
-    answer = answer & 0xffff
-
-    # Swap bytes. Bugger me if I know why.
-    answer = answer >> 8 | (answer << 8 & 0xff00)
-
-    return answer
+    for i in range(0, len(source), 2):
+        sum = ones_comp_sum16(sum, (source[i + 1] << 8) + source[i])
+    return ~sum & 0xffff
 
 
-def receive_one_ping(my_socket, ID, timeout):
+def send_one_ping(sock: socket, dest_addr: str, icmp_id: int, seq: int, size: int):
+    """Sends one ping to the given destination.
+
+    ICMP Header (bits): type (8), code (8), checksum (16), id (16), sequence (16)
+    ICMP Payload: time (double), data
+    ICMP Wikipedia: https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol
+
+    Args:
+        sock: Socket.
+        dest_addr: The destination address, can be an IP address or a domain name. Ex. "192.168.1.1"/"example.com"
+        icmp_id: ICMP packet id, usually is same as pid.
+        seq: ICMP packet sequence, usually increases from 0 in the same process.
+        size: The ICMP packet payload size in bytes. Note this is only for the payload part.
+
+    Raises:
+        HostUnkown: If destination address is a domain name and cannot resolved.
     """
-    receive the ping from the socket.
+    try:
+        dest_addr = socket.gethostbyname(dest_addr)  # Domain name will translated into IP address, and IP address leaves unchanged.
+    except socket.gaierror as e:
+        print("Cannot resolve {}: Unknown host".format(dest_addr))
+        raise errors.HostUnknown(dest_addr) from e
+    pseudo_checksum = 0  # Pseudo checksum is used to calculate the real checksum.
+    icmp_header = struct.pack(ICMP_HEADER_FORMAT, IcmpType.ECHO_REQUEST, ICMP_DEFAULT_CODE, pseudo_checksum, icmp_id, seq)
+    padding = (size - struct.calcsize(ICMP_TIME_FORMAT) - struct.calcsize(ICMP_HEADER_FORMAT)) * "Q"  # Using double to store current time.
+    icmp_payload = struct.pack(ICMP_TIME_FORMAT, default_timer()) + padding.encode()
+    real_checksum = checksum(icmp_header + icmp_payload)  # Calculates the checksum on the dummy header and the icmp_payload.
+    # Don't know why I need socket.htons() on real_checksum since ICMP_HEADER_FORMAT already in Network Bytes Order (big-endian)
+    icmp_header = struct.pack(ICMP_HEADER_FORMAT, IcmpType.ECHO_REQUEST, ICMP_DEFAULT_CODE, socket.htons(real_checksum), icmp_id, seq)  # Put real checksum into ICMP header.
+    packet = icmp_header + icmp_payload
+    sock.sendto(packet, (dest_addr, 0))  # addr = (ip, port). Port is 0 respectively the OS default behavior will be used.
+
+
+def receive_one_ping(sock: socket, icmp_id: int, seq: int, timeout: int) -> float or None:
+    """Receives the ping from the socket.
+
+    IP Header (bits): version (8), type of service (8), length (16), id (16), flags (16), time to live (8), protocol (8), checksum (16), source ip (32), destination ip (32).
+    ICMP Packet (bytes): IP Header (20), ICMP Header (8), ICMP Payload (*).
+    Ping Wikipedia: https://en.wikipedia.org/wiki/Ping_(networking_utility)
+    ToS (Type of Service) in IP header for ICMP is 0. Protocol in IP header for ICMP is 1.
+
+    Args:
+        sock: The same socket used for send the ping.
+        icmp_id: ICMP packet id. Sent packet id should be identical with received packet id.
+        seq: ICMP packet sequence. Sent packet sequence should be identical with received packet sequence.
+        timeout: Timeout in seconds.
+
+    Returns:
+        The delay in seconds or None on timeout.
+
+    Raises:
+        TimeToLiveExpired: If the Time-To-Live in IP Header is not large enough for destination.
+        TimeExceeded: If time exceeded but Time-To-Live does not expired.
     """
-    timeLeft = timeout
+    ip_header_slice = slice(0, struct.calcsize(IP_HEADER_FORMAT))  # [0:20]
+    icmp_header_slice = slice(ip_header_slice.stop, ip_header_slice.stop + struct.calcsize(ICMP_HEADER_FORMAT))  # [20:28]
+    ip_header_keys = ('version', 'tos', 'len', 'id', 'flags', 'ttl', 'protocol', 'checksum', 'src_addr', 'dest_addr')
+    icmp_header_keys = ('type', 'code', 'checksum', 'id', 'seq')
     while True:
-        startedSelect = default_timer()
-        whatReady = select.select([my_socket], [], [], timeLeft)
-        howLongInSelect = (default_timer() - startedSelect)
-        if whatReady[0] == []:  # Timeout
-            return
-
-        timeReceived = default_timer()
-        recPacket, addr = my_socket.recvfrom(1024)
-        icmpHeader = recPacket[20:28]
-        type, code, checksum, packetID, sequence = struct.unpack(
-            "bbHHh", icmpHeader
-        )
-        # Filters out the echo request itself.
-        # This can be tested by pinging 127.0.0.1
-        # You'll see your own request
-        if type != 8 and packetID == ID:
-            bytesInDouble = struct.calcsize("d")
-            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
-            return timeReceived - timeSent
-
-        timeLeft = timeLeft - howLongInSelect
-        if timeLeft <= 0:
-            return
+        selected = select.select([sock], [], [], timeout)
+        if selected[0] == []:  # Timeout
+            raise errors.Timeout(timeout)
+        time_recv = default_timer()
+        recv_data, addr = sock.recvfrom(1024)
+        ip_header_raw, icmp_header_raw, icmp_payload_raw = recv_data[ip_header_slice], recv_data[icmp_header_slice], recv_data[icmp_header_slice.stop:]
+        ip_header = dict(zip(ip_header_keys, struct.unpack(IP_HEADER_FORMAT, ip_header_raw)))
+        _debug("IP HEADER:", ip_header)
+        icmp_header = dict(zip(icmp_header_keys, struct.unpack(ICMP_HEADER_FORMAT, icmp_header_raw)))
+        _debug("ICMP HEADER:", icmp_header)
+        if icmp_header['type'] == IcmpType.TIME_EXCEEDED:  # TIME_EXCEEDED has no icmp_id and icmp_seq. Usually they are 0.
+            if icmp_header['code'] == IcmpTimeExceededCode.TTL_EXPIRED:
+                raise errors.TimeToLiveExpired()  # Some router does not report TTL expired and then timeout shows.
+            raise errors.TimeExceeded()
+        if icmp_header['id'] == icmp_id or icmp_header['seq'] == seq:  # ECHO_REPLY should match the
+            if icmp_header['type'] == IcmpType.ECHO_REQUEST:  # filters out the ECHO_REQUEST itself.
+                _debug("ECHO_REQUEST filtered out.")
+                continue
+            if icmp_header['type'] == IcmpType.ECHO_REPLY:
+                time_sent = struct.unpack(ICMP_TIME_FORMAT, icmp_payload_raw[0:struct.calcsize(ICMP_TIME_FORMAT)])[0]
+                return time_recv - time_sent
 
 
-def send_one_ping(my_socket, dest_addr, ID):
-    """
-    Send one ping to the given >dest_addr<.
-    """
-    dest_addr = socket.gethostbyname(dest_addr)
-
-    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
-    my_checksum = 0
-
-    # Make a dummy header with a 0 checksum.
-    # ID: Low-endian identifier, bbHHh: network byte order
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
-    bytesInDouble = struct.calcsize("d")
-    data = (192 - bytesInDouble) * "Q"
-    data = struct.pack("d", default_timer()) + data.encode()
-
-    # Calculate the checksum on the data and the dummy header.
-    my_checksum = checksum(header + data)
-
-    # Now that we have the right checksum, we put that in. It's just easier
-    # to make up a new header than to stuff it into the dummy.
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1)
-    packet = header + data
-    my_socket.sendto(packet, (dest_addr, 1))  # Don't know about the 1
-
-
-def ping(dest_addr, timeout=4, unit="s", src_addr=None, ttl=64):
+def ping(dest_addr: str, timeout: int = 4, unit: str = "s", src_addr: str = None, ttl: int = 64, seq: int = 0, size: int = 56) -> float or None:
     """
     Send one ping to destination address with the given timeout.
 
     Args:
-        dest_addr: Str. The destination address. Ex. "192.168.1.1"/"example.com"
-        timeout: Int. Timeout in seconds. Default is 4s, same as Windows CMD.
-        unit: Str. The unit of returned value. Default is "s" for seconds, "ms" for milliseconds.
-        src_addr: Str. The IP address to ping from. Ex. "192.168.1.20"
-        ttl: Int. The Time-To-Live of the outgoing packet. Default is 64, same as in Linux and macOS.
+        dest_addr: The destination address, can be an IP address or a domain name. Ex. "192.168.1.1"/"example.com"
+        timeout: Timeout in seconds. Default is 4s, same as Windows CMD. (default 4)
+        unit: The unit of returned value. "s" for seconds, "ms" for milliseconds. (default "s")
+        src_addr: The IP address to ping from. This is for multi-interface clients. Ex. "192.168.1.20". (default None)
+        ttl: The Time-To-Live of the outgoing packet. Default is 64, same as in Linux and macOS. (default 64)
+        seq: ICMP packet sequence, usually increases from 0 in the same process. (default 0)
+        size: The ICMP packet payload size in bytes. Default is 56, same as in macOS. (default 56)
 
     Returns:
         The delay in seconds/milliseconds or None on timeout.
+
+    Raises:
+        PingError: Any PingError will raise again if `ping3.EXCEPTIONS` is True.
     """
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    my_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-    if src_addr:
-        my_socket.bind((src_addr, 0))
-    my_ID = threading.current_thread().ident & 0xFFFF
-    send_one_ping(my_socket, dest_addr, my_ID)
-    delay = receive_one_ping(my_socket, my_ID, timeout)  # in seconds
-    my_socket.close()
-    if delay is None:
-        return None
-    if unit == "ms":
-        delay *= 1000  # in milliseconds
+    with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
+        sock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        if src_addr:
+            sock.bind((src_addr, 0))
+        icmp_id = threading.current_thread().ident & 0xFFFF
+        try:
+            send_one_ping(sock=sock, dest_addr=dest_addr, icmp_id=icmp_id, seq=seq, size=size)
+            delay = receive_one_ping(sock=sock, icmp_id=icmp_id, seq=seq, timeout=timeout)  # in seconds
+        except errors.PingError as e:
+            _debug(e)
+            if EXCEPTIONS:
+                raise e
+            return None
+        if delay is None:
+            return None
+        if unit == "ms":
+            delay *= 1000  # in milliseconds
     return delay
 
 
-def verbose_ping(dest_addr, count=4, *args, **kwargs):
+def verbose_ping(dest_addr: str, count: int = 4, *args, **kwargs):
     """
     Send pings to destination address with the given timeout and display the result.
 
     Args:
-        dest_addr: Str. The destination address. Ex. "192.168.1.1"/"example.com"
-        count: Int. How many pings should be sent. Default is 4, same as Windows CMD.
-        *: And all the other arguments available in ping().
+        dest_addr: The destination address. Ex. "192.168.1.1"/"example.com"
+        count: How many pings should be sent. Default is 4, same as Windows CMD. (default 4)
+        *args and **kwargs: And all the other arguments available in ping() except `seq`.
 
     Returns:
         Formatted ping results printed.
     """
     timeout = kwargs.get("timeout")
-    src_addr = kwargs.get("src_addr")
+    src = kwargs.get("src")
     unit = kwargs.setdefault("unit", "ms")
     for i in range(count):
         output_text = "ping '{}'".format(dest_addr)
-        output_text += " from '{}'".format(src_addr) if src_addr else ""
+        output_text += " from '{}'".format(src) if src else ""
         output_text += " ... "
         print(output_text, end="")
-        try:
-            delay = ping(dest_addr, *args, **kwargs)
-        except socket.gaierror as e:
-            print("Failed. (socket error: '{}')".format(e))
-            break
+        delay = ping(dest_addr, seq=i, *args, **kwargs)
         if delay is None:
             print("Timeout > {}s".format(timeout) if timeout else "Timeout")
         else:
             print("{value}{unit}".format(value=int(delay), unit=unit))
-    print
 
 
 if __name__ == "__main__":
