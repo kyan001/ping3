a89f3d8b45427cb34418b8a830c89f8013f3c49d
Kyan <ykyankai@gmail.com>
2019-06-15 02:42:34 +0800

    separate command_line executing and ping3 codes.

---
+++ command_line_ping3.py
@@ -0,0 +1,25 @@
+import argparse
+
+import ping3
+
+
+def main(assigned_args: list = None):
+    """
+    Parse and execute the call from command-line.
+
+    Args:
+        assigned_args: List of strings to parse. The default is taken from sys.argv.
+
+    Returns:
+        Formatted ping results printed.
+    """
+    parser = argparse.ArgumentParser(prog="ping3", description="A pure python3 version of ICMP ping implementation using raw socket.", epilog="!!Note: ICMP messages can only be sent from processes running as root.")
+    parser.add_argument("-v", "--version", action="version", version=ping3.__version__)
+    parser.add_argument(dest="dest_addr", metavar="DEST_ADDR", nargs="*", default=("example.com", "8.8.8.8"), help="The destination address, can be an IP address or a domain name. Ex. 192.168.1.1/example.com.")
+    parser.add_argument("-c", "--count", dest="count", metavar="COUNT", type=int, default=4, help="How many pings should be sent. Default is 4.")
+    parser.add_argument("-w", "--wait", dest="timeout", metavar="TIMEOUT", type=float, default=4, help="Time to wait for a response, in seconds. Default is 4.")
+    parser.add_argument("-t", "--ttl", dest="ttl", metavar="TTL", type=int, default=64, help="The Time-To-Live of the outgoing packet. Default is 64.")
+    parser.add_argument("-l", "--load", dest="size", metavar="SIZE", type=int, default=56, help="The ICMP packet payload size in bytes. Default is 56.")
+    args = parser.parse_args(assigned_args)
+    for addr in args.dest_addr:
+        ping3.verbose_ping(addr, count=args.count, ttl=args.ttl, timeout=args.timeout, size=args.size)
---
+++ tests/test_command_line_ping3.py
@@ -0,0 +1,60 @@
+import sys
+import os.path
+import io
+import unittest
+from unittest.mock import patch
+
+sys.path.insert(0, os.path.dirname(sys.path[0]))
+import command_line_ping3  # noqa: linter (pycodestyle) should not lint this line.
+
+
+class test_ping3(unittest.TestCase):
+    """command-line ping3 unittest"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_dest_addr_0(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main()
+            self.assertRegex(fake_out.getvalue(), r".*[0-9]+ms.*")
+
+    def test_dest_addr_1(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main(["127.0.0.1"])
+            self.assertTrue("127.0.0.1" in fake_out.getvalue())
+
+    def test_dest_addr_2(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main(["127.0.0.1", "8.8.8.8"])
+            self.assertTrue("127.0.0.1" in fake_out.getvalue())
+            self.assertTrue("8.8.8.8" in fake_out.getvalue())
+
+    def test_count(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main(['-c', '1', 'example.com'])
+            self.assertEqual(fake_out.getvalue().count("\n"), 1)
+
+    def test_timeout(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main(['-w', '0.0001', 'example.com'])
+            self.assertRegex(fake_out.getvalue(), r".*Timeout \> [0-9\.]+s.*")
+
+    def test_ttl(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main(['-t', '0', 'example.com'])
+            self.assertRegex(fake_out.getvalue(), r".*Timeout.*")
+
+    def test_size(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line_ping3.main(['-l', '100', 'example.com'])
+            self.assertRegex(fake_out.getvalue(), r".*[0-9]+ms.*")
+            with self.assertRaises(OSError):
+                command_line_ping3.main(['-l', '99999', 'example.com'])
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2, exit=False)
