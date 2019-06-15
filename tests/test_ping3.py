e96b254e50d168c01dcf6a602733002f36459e65
Kyan <ykyankai@gmail.com>
2019-06-15 23:41:55 +0800

    fix typo in setup.py

--- tests/test_ping3.py
+++ tests/test_ping3.py
@@ -12,7 +12,7 @@ import errors  # noqa: linter (pycodestyle) should not lint this line.
 
 class test_ping3(unittest.TestCase):
     """ping3 unittest"""
-    __version__ = "2.2.2"
+    __version__ = "2.2.3"
 
     def setUp(self):
         pass
