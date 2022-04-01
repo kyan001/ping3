4bc1d513a61fc8f7af62232df6bc505523cb3dda
patrik arlos <patrik.arlos@bth.se>
2022-04-01 12:56:27 +0200

    Added error handling in verbose_print, in case EXTENDED was used and the ping failed for some reason (timeout, error, etc..)

--- ping3/__init__.py
+++ ping3/__init__.py
@@ -15,7 +15,7 @@ import errno
 from . import errors
 from .enums import ICMP_DEFAULT_CODE, IcmpType, IcmpTimeExceededCode, IcmpDestinationUnreachableCode
 
-__version__ = "4.0.0"
+__version__ = "4.0.0E"
 DEBUG = False  # DEBUG: Show debug info for developers. (default False)
 EXCEPTIONS = False  # EXCEPTIONS: Raise exception when delay is not available.
 LOGGER = None  # LOGGER: Record logs into console or file.
@@ -29,6 +29,9 @@ SOCKET_SO_BINDTODEVICE = 25  # socket.SO_BINDTODEVICE
 class myData:
     version = __version__ + "E"
     destip = "NA"
+    destport= 0
+    srcip = "NA"
+    srcport = 0
     timeout = 0
     roundtriptime = 0
     requests = 0
@@ -334,7 +337,9 @@ def ping(dest_addr: str, timeout: int = 4, unit: str = "s", src_addr: str = None
             delay *= 1000  # in milliseconds
 
         if EXTENDED == True:
-            resp.destip=str(dest_addr)
+            resp.destip=str(socket.gethostbyname(dest_addr))
+            resp.srcip=str(sock.getsockname()[0])
+            resp.srcport=sock.getsockname()[1]
             resp.timeout=timeout
             resp.roundtriptime=delay
             resp.requests=1
@@ -343,82 +348,7 @@ def ping(dest_addr: str, timeout: int = 4, unit: str = "s", src_addr: str = None
             
         return delay
 
-@_func_logger
-def pingE(dest_addr: str, timeout: int = 4, unit: str = "s", src_addr: str = None, ttl: int = None, seq: int = 0, size: int = 56, interface: str = None) -> float:
-    """
-    Send one ping to destination address with the given timeout, return an object with the information.
 
-    Args:
-        dest_addr: The destination address, can be an IP address or a domain name. Ex. "192.168.1.1"/"example.com"
-        timeout: Time to wait for a response, in seconds. Default is 4s, same as Windows CMD. (default 4)
-        unit: The unit of returned value. "s" for seconds, "ms" for milliseconds. (default "s")
-        src_addr: The IP address to ping from. This is for multiple network interfaces. Ex. "192.168.1.20". (default None)
-        interface: LINUX ONLY. The gateway network interface to ping from. Ex. "wlan0". (default None)
-        ttl: The Time-To-Live of the outgoing packet. Default is None, which means using OS default ttl -- 64 onLinux and macOS, and 128 on Windows. (default None)
-        seq: ICMP packet sequence, usually increases from 0 in the same process. (default 0)
-        size: The ICMP packet payload size in bytes. If the input of this is less than the bytes of a double format (usually 8), the size of ICMP packet payload is 8 bytes to hold a time. The max should be the router_MTU(Usually 1480) - IP_Header(20) - ICMP_Header(8). Default is 56, same as in macOS. (default 56)
-
-    Returns:
-        The delay in seconds/milliseconds, False on error and None on timeout.
-
-    Raises:
-        PingError: Any PingError will raise again if `ping3.EXCEPTIONS` is True.
-    """
-    resp=myData()
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    except PermissionError as err:
-        if err.errno == errno.EPERM:  # [Errno 1] Operation not permitted
-            _debug("`{}` when create socket.SOCK_RAW, using socket.SOCK_DGRAM instead.".format(err))
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_ICMP)
-        else:
-            raise err
-    with sock:
-        if ttl:
-            try:  # IPPROTO_IP is for Windows and BSD Linux.
-                if sock.getsockopt(socket.IPPROTO_IP, socket.IP_TTL):
-                    sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
-            except OSError as err:
-                _debug("Set Socket Option `IP_TTL` in `IPPROTO_IP` Failed: {}".format(err))
-            try:
-                if sock.getsockopt(socket.SOL_IP, socket.IP_TTL):
-                    sock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-            except OSError as err:
-                _debug("Set Socket Option `IP_TTL` in `SOL_IP` Failed: {}".format(err))
-        if interface:
-            sock.setsockopt(socket.SOL_SOCKET, SOCKET_SO_BINDTODEVICE, interface.encode())  # packets will be sent from specified interface.
-            _debug("Socket Interface Binded:", interface)
-        if src_addr:
-            sock.bind((src_addr, 0))  # only packets send to src_addr are received.
-            _debug("Socket Source Address Binded:", src_addr)
-        thread_id = threading.get_native_id() if hasattr(threading, 'get_native_id') else threading.currentThread().ident  # threading.get_native_id() is supported >= python3.8.
-        process_id = os.getpid()  # If ping() run under different process, thread_id may be identical.
-        icmp_id = zlib.crc32("{}{}".format(process_id, thread_id).encode()) & 0xffff  # to avoid icmp_id collision.
-        try:
-            send_one_ping(sock=sock, dest_addr=dest_addr, icmp_id=icmp_id, seq=seq, size=size)
-            delay = receive_one_ping(sock=sock, icmp_id=icmp_id, seq=seq, timeout=timeout)  # in seconds
-        except errors.Timeout as err:
-            _debug(err)
-            _raise(err)
-            return None
-        except errors.PingError as err:
-            _debug(err)
-            _raise(err)
-            return False
-        if delay is None:
-            return None
-        if unit == "ms":
-            delay *= 1000  # in milliseconds
-
-        if EXTENDED == True:
-            resp.destip=str(dest_addr)
-            resp.timeout=timeout
-            resp.roundtriptime=delay
-            resp.requests=1
-            resp.success=True
-            return resp
-        
-        return delay
 
 @_func_logger
 def verbose_ping(dest_addr: str, count: int = 4, interval: float = 0, *args, **kwargs):
@@ -434,6 +364,7 @@ def verbose_ping(dest_addr: str, count: int = 4, interval: float = 0, *args, **k
     Returns:
         Formatted ping results printed.
     """
+    resp=myData()
     timeout = kwargs.get("timeout")
     src = kwargs.get("src")
     unit = kwargs.setdefault("unit", "ms")
@@ -444,12 +375,24 @@ def verbose_ping(dest_addr: str, count: int = 4, interval: float = 0, *args, **k
         output_text = "ping '{}'".format(dest_addr)
         output_text += " from '{}'".format(src) if src else ""
         output_text += " ... "
-        delay = ping(dest_addr, seq=i, *args, **kwargs)
+        data = ping(dest_addr, seq=i, *args, **kwargs)
+        
+        if EXTENDED == True and isinstance(data, myData) :
+            print("[EXTENDED] ", end="")
+            delay=data.roundtriptime
+        else:
+            delay=data
+
+            
         print(output_text, end="")
         if delay is None:
             print("Timeout > {}s".format(timeout) if timeout else "Timeout")
         elif delay is False:
             print("Error")
         else:
-            print("{value}{unit}".format(value=int(delay), unit=unit))
+            if EXTENDED == True:
+                print("{value}{unit}".format(value=int(delay), unit=unit),end='')
+                print(" " + data.srcip + " -> " + data.destip + "."  )
+            else:
+                print("{value}{unit}".format(value=int(delay), unit=unit))
         i += 1
