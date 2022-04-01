ec71fe256fbfe7fc854b1abfc11fdc0d8eb9f400
patrik arlos <patrik.arlos@bth.se>
2022-04-01 13:17:45 +0200

    Fixed the command_line version to work with EXTENDED (-X). Fixed corresponding testing as well.

--- ping3/command_line.py
+++ ping3/command_line.py
@@ -15,7 +15,7 @@ def main(assigned_args: list = None):
     """
     parser = argparse.ArgumentParser(prog="ping3", description="A pure python3 version of ICMP ping implementation using raw socket.", epilog="!!Note: ICMP messages can only be sent from processes running as root.")
     parser.add_argument("-v", "--version", action="version", version=ping3.__version__)
-    parser.add_argument(dest="dest_addr", metavar="DEST_ADDR", nargs="*", default=("example.com", "8.8.8.8"), help="The destination address, can be an IP address or a domain name. Ex. 192.168.1.1/example.com.")
+    parser.add_argument(dest="dest_addr", metavar="DEST_ADDR", nargs="*", default=("dns.google", "8.8.8.8"), help="The destination address, can be an IP address or a domain name. Ex. 192.168.1.1/example.com.")
     parser.add_argument("-c", "--count", dest="count", metavar="COUNT", type=int, default=4, help="How many pings should be sent. Default is 4.")
     parser.add_argument("-t", "--timeout", dest="timeout", metavar="TIMEOUT", type=float, default=4, help="Time to wait for a response, in seconds. Default is 4.")
     parser.add_argument("-i", "--interval", dest="interval", metavar="INTERVAL", type=float, default=0, help="Time to wait between each packet, in seconds. Default is 0.")
@@ -29,6 +29,7 @@ def main(assigned_args: list = None):
     args = parser.parse_args(assigned_args)
     ping3.DEBUG = args.debug
     ping3.EXCEPTIONS = args.exceptions
+    ping3.EXTENDED = args.extended
 
     for addr in args.dest_addr:
         ping3.verbose_ping(addr, count=args.count, ttl=args.ttl, timeout=args.timeout, size=args.size, interval=args.interval, interface=args.interface, src_addr=args.src_addr, )
--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -38,37 +38,47 @@ class test_ping3(unittest.TestCase):
 
     def test_count(self):
         with patch("sys.stdout", new=io.StringIO()) as fake_out:
-            command_line.main(["-c", "1", "example.com"])
+            command_line.main(["-c", "1", "dns.google"])
             self.assertEqual(fake_out.getvalue().count("\n"), 1)
 
     def test_timeout(self):
         with patch("sys.stdout", new=io.StringIO()) as fake_out:
-            command_line.main(["-t", "0.0001", "example.com"])
+            command_line.main(["-t", "0.0001", "dns.google"])
             self.assertRegex(fake_out.getvalue(), r".*Timeout \> [0-9\.]+s.*")
 
     @unittest.skipIf(sys.platform.startswith("win"), "Linux and macOS Only")
     def test_ttl(self):
         with patch("sys.stdout", new=io.StringIO()) as fake_out:
-            command_line.main(["-T", "1", "example.com"])
-            self.assertRegex(fake_out.getvalue(), r".*Error.*")
+            command_line.main(["-T", "1", "dns.google"])
+            self.assertRegex(fake_out.getvalue(), r".*Timeout.*")
+
+    @unittest.skipIf(sys.platform.startswith("win"), "Linux and macOS Only")
+    def test_ttl2(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line.main(["-T", "1", "dns.google"])
+            self.assertRegex(fake_out.getvalue(), r".*Timeout.*")     
 
     def test_size(self):
         with patch("sys.stdout", new=io.StringIO()) as fake_out:
-            command_line.main(["-s", "100", "example.com"])
+            command_line.main(["-s", "100", "dns.google"])
             self.assertRegex(fake_out.getvalue(), r".*[0-9]+ms.*")
             with self.assertRaises(OSError):
-                command_line.main(["-s", "99999", "example.com"])
+                command_line.main(["-s", "99999", "dns.google"])
 
     def test_interval(self):
         with patch("sys.stdout", new=io.StringIO()) as fake_out:
             start_time = time.time()
-            command_line.main(["-i", "1", "example.com"])
+            command_line.main(["-i", "1", "dns.google"])
             end_time = time.time()
             self.assertTrue((end_time - start_time) >= 3)  # time_expect = (count - 1) * interval
             self.assertNotIn("Timeout", fake_out.getvalue())
 
     @unittest.skipUnless(sys.platform == "linux", "Linux only")
     def test_interface(self):
+        if os.geteuid() != 0:
+            print("Needs sudo.")
+            self.fail("Need sudo.");
+            
         with patch("sys.stdout", new=io.StringIO()) as fake_out:
             try:
                 route_cmd = os.popen("ip -o -4 route show to default")
@@ -80,7 +90,7 @@ class test_ping3(unittest.TestCase):
                 socket.if_nametoindex(my_interface)  # test if the interface exists.
             except OSError:
                 self.fail("Interface Name Error: {}".format(my_interface))
-            command_line.main(["-I", my_interface, "example.com"])
+            command_line.main(["-I", my_interface, "dns.google"])
             self.assertRegex(fake_out.getvalue(), r".*[0-9]+ms.*")
 
     def test_src_addr(self):
@@ -89,18 +99,24 @@ class test_ping3(unittest.TestCase):
             if my_ip in ("127.0.0.1", "127.0.1.1"):  # This may caused by /etc/hosts settings.
                 dest_addr = my_ip  # only localhost can send and receive from 127.0.0.1 (or 127.0.1.1 on Ubuntu).
             else:
-                dest_addr = "example.com"
+                dest_addr = "dns.google"
             command_line.main(["-S", my_ip, dest_addr])
             self.assertRegex(fake_out.getvalue(), r".*[0-9]+ms.*")
 
     def test_debug(self):
         with patch("sys.stdout", new=io.StringIO()), patch("sys.stderr", new=io.StringIO()) as fake_err:
-            command_line.main(["--debug", "-c", "1", "example.com"])
+            command_line.main(["--debug", "-c", "1", "dns.google"])
             self.assertIn("[DEBUG]", fake_err.getvalue())
 
+    def test_extended(self):
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            command_line.main(["--eXtended", "-c", "1", "dns.google"])
+            self.assertIn("[EXTENDED]", fake_out.getvalue())
+
+            
     def test_exceptions(self):
         with self.assertRaises(errors.Timeout):
-            command_line.main(["--exceptions", "-t", "0.0001", "example.com"])
+            command_line.main(["--exceptions", "-t", "0.0001", "dns.google"])
 
 
 if __name__ == "__main__":
